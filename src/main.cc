fa9ab991f78ea475b74939dfcc656a9cd051f1d0
Jamie Hernandez <litoxl@hotmail.com>
2019-12-13 16:29:11 -0800

    EXCELSIOR!!!

--- src/main.cc
+++ src/main.cc
@@ -13,12 +13,12 @@
 int main(int argc, char *argv[]) {
     std::vector<int> ints{10,9,8,7,6,5,4,3,2,1};
 
-    auto pivot = edu::vcccd::vc::csv15::partition(ints.begin(), ints.end(), 5);
+    auto pivot = edu::vcccd::vc::csv15::partition(ints.begin(), ints.end(), 6);
     for (int val: ints) {
         std::cout << val << std::endl;
     }
 
-    if (std::is_partitioned(ints.begin(), ints.end(), [&] (int v) -> bool { return v <= 5; })) {
+    if (std::is_partitioned(ints.begin(), ints.end(), [&] (int v) -> bool { return v <= 6; })) {
         std::cout << "partitioned" << std::endl;
     } else {
         std::cout << "non-partitioned" << std::endl;
